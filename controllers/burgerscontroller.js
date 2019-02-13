35db8a22324bd0cdb962188a20139abf51840e37
Keiran Hamilton <keiranhamilton@gmail.com>
2019-02-13 09:32:23 -0500

    heroku update

--- controllers/burgerscontroller.js
+++ controllers/burgerscontroller.js
@@ -15,11 +15,11 @@ router.get("/burgers", function(req, res) {
   });
 });
 
-// post route -> back to index
+// post route  back to index
 router.post("/burgers/create", function(req, res) {
-  // takes the request object using it as input for burger.addBurger
+  // takes the object using it as input for burger.addBurger
   burger.create(req.body.burger_name, function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // wrapper for orm.js  using MySQL/ a callback will return a log to console,
     // render back to index with handle
     console.log(result);
     res.redirect("/");
